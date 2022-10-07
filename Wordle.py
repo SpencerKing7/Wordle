b820662196b0d5c726b49599c026002978593658
logankimball <logankimbs@gmail.com>
2022-10-06 19:28:03 -0600

    fixed bug that prevented user from seeing letters when typing. added colors to keyboard

--- Wordle.py
+++ Wordle.py
@@ -3,13 +3,11 @@
 """
 WORDLE 
 Abbie Luper, Spencer King, Logan Kimball, Samantha Prettyman, Zachary Barton
-
 """
 
 import random
-
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
+from WordleGraphics import WordleGWindow, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR
 
 def wordle():
     wordleWord = random.choice(FIVE_LETTER_WORDS)
@@ -17,38 +15,50 @@ def wordle():
 
     print(wordleWord)
 
+    def is_unknown(x):
+        return True if gw.get_key_color(x) == UNKNOWN_COLOR else False
+
     def enter_action(guess):
+        hasWon = False
         i = 0
 
         # SEE IF THE WORD IS LEGITIMATE
         if guess.lower() in FIVE_LETTER_WORDS:
             print("Hurray! Guess " + guess + " was found in word list")
 
-            # CHANGE COLOR OF BOXES IF WORD IS LEGITIMATE
-            # for col in range(5):
-            #     if gw.get_square_letter(gw.get_current_row(), col).lower() == wordleWord[col]:
-            #         gw.set_square_color(gw.get_current_row(), col, CORRECT_COLOR)
-            #     elif gw.get_square_letter(gw.get_current_row(), col).lower() in wordleWord:
-            #         gw.set_square_color(gw.get_current_row(), col, PRESENT_COLOR)
-            #     else:
-            #         gw.set_square_color(gw.get_current_row(), col, MISSING_COLOR) 
-
-            for letter in guess:
-                if letter == guess[i].lower():
-                    gw.set_square_color(gw.get_current_row(), i, CORRECT_COLOR)
-
             # MESSAGE FOR WHEN THEY GET THE CORRECT WORD
-            if guess.lower() == wordleWord.lower():
+            if (guess.lower() == wordleWord):
+                hasWon = True
                 gw.show_message("Congrats! You guessed the word!")
             
-            gw.set_current_row(gw.get_current_row() + 1)
+            # CHANGE COLOR OF BOXES IF WORD IS LEGITIMATE
+            for letter in wordleWord:
+                if letter == guess[i].lower():
+                    gw.set_square_color(gw.get_current_row(), i, CORRECT_COLOR)
+                    
+                    if is_unknown(guess[i]):
+                        gw.set_key_color(guess[i], CORRECT_COLOR)
+                elif guess[i].lower() in wordleWord:
+                    gw.set_square_color(gw.get_current_row(), i, PRESENT_COLOR)
+                    
+                    if is_unknown(guess[i]):
+                        gw.set_key_color(guess[i], PRESENT_COLOR)
+                else:
+                    gw.set_square_color(gw.get_current_row(), i, MISSING_COLOR)
+                    
+                    if is_unknown(guess[i]):
+                        gw.set_key_color(guess[i], MISSING_COLOR)
+                
+                i += 1
+            
+            # INCREMENT ROW
+            if not hasWon:
+                gw.set_current_row(gw.get_current_row() + 1)
         else:
             gw.show_message("Not in word list.")
     
     gw.set_current_row(0)
     gw.add_enter_listener(enter_action)
-
-# Startup code
 
 if __name__ == "__main__":
     wordle()
